f05d1470d2f92a00aa3aa02ada741221ec99ab9c
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2023-07-23 17:34:07 +0530

    fw27_017 day-5

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-let loginBtn=document.getElementById('loginBtn');
+let body = document.body;
+let cta = document.getElementById("cta");
+
+cta.addEventListener("click", () => {
+  displayMessage("Submission Successfull!", "success");
+});
+
+function displayMessage(str, type) {
+  let msgCard = document.createElement("div");
+  msgCard.innerText = str;
+  msgCard.classList.add("msg-card");
+  if (type == "success") {
+    msgCard.classList.add("success");
+  } else if (type == "warning") {
+    msgCard.classList.add("warning");
+  }
+  body.append(msgCard);
+  setTimeout(function () {
+    msgCard.style.display = "none";
+    body.removeChild(msgCard);
+  }, 2900);
+}
+
+// displayMessage('Testing Message','success');//Success message
+// displayMessage('Testing Message','warning');//Error Message
+
+// // Toggle Swtich Functionality
+// let toggleBtn= document.querySelector('#toggle input[type="checkbox"]');
+
+// toggleBtn.addEventListener('click',()=>{
+//   if(toggleBtn.checked){
+//     document.body.classList.add('light-mode');
+//   }else{
+//     document.body.classList.remove('light-mode');
+//   }
+// })
